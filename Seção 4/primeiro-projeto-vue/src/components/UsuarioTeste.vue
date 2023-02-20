9f8c70bf7a1a4078b6119df1fa4d01ef0086967e
Leonardo Selegar <leonardo11132@gmail.com>
2023-02-19 22:10:16 -0300

    Primeiros componentes

---
+++ Seção 4/primeiro-projeto-vue/src/components/UsuarioTeste.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="UsuarioTeste">
+        <h2>Olá: Sujeito Programador</h2>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UsuarioTeste'
+}
+</script>
+
+<style>
+    h2{
+        color: red;
+    }
+</style>
