39d64f0222b9bec3e0f8333d1ad005c9a036537a
Leonardo Selegar <leonardo11132@gmail.com>
2023-02-21 20:40:00 -0300

    Props no vue.js

---
+++ Seção 5/props/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Bem vindo ao meu primeiro projeto!</h1>
+    <button @click="entrar">Entrar</button>
+    <UsuarioTeste :nome="nome"/>
+  </div>
+</template>
+
+<script>
+import UsuarioTeste from './components/UsuarioTeste.vue';
+
+  export default{
+    name: 'App',
+    components:{
+      UsuarioTeste
+    },
+    data(){
+      return{
+        nome:'Sujeito Programador'
+      }
+    },
+    methods: {
+      entrar() {
+        this.nome = 'Curso Vue JS';
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ Seção 5/props/src/components/UsuarioTeste.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="UsuarioTeste">
+        <h2>Olá: {{nome}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UsuarioTeste',
+    props: ['nome']
+}
+</script>
+
+<style scoped> /* esse css só funciona no Usuario*/
+    h2{
+        color: red;
+    }
+</style>
