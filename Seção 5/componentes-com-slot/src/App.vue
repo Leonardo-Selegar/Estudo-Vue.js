ab370d02f61e5fc2385d63157863346b367cb5a1
Leonardo Selegar <leonardo11132@gmail.com>
2023-02-23 02:13:14 -0300

    Primeira parte slots

---
+++ Seção 5/componentes-com-slot/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Componente App :)</h1>
+    <ComponentUsuario>
+      <strong>Bem vindo!!!</strong>
+      <p>Meu nome é: {{ nome }}</p>
+    </ComponentUsuario>
+  </div>
+</template>
+
+<script>
+import ComponentUsuario from './components/Usuario.vue';
+export default {
+  name: 'App',
+  components:{
+    ComponentUsuario,
+  },
+  data(){
+    return{
+      nome: 'Sujeito Programador'
+    }
+  }
+}
+</script>
+
+<style scoped>
+ p{
+  color: blue;
+ }
+</style>
---
+++ Seção 5/componentes-com-slot/src/components/Usuario.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="ComponentUsuario">
+    <h1>Componente App :)</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentUsuario',
+}
+</script>
+
+<style scoped>
+  p{
+    color: red;
+  }
+</style>
