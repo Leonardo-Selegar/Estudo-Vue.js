57accb1648f4cf4df9f862c196e891328d24683d
Leonardo Selegar <leonardo11132@gmail.com>
2023-02-23 23:13:07 -0300

    Submit no vue

--- Seção 6/secao-seis/src/App.vue
+++ Seção 6/secao-seis/src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <h1>Fazer Cadastro</h1>
     <div>
-      <form action="">
-        <label for="">Nome </label>
-        <input type="text"  v-model="nome"><br>
+      <form @submit="enviar">
+        <label>Nome </label>
+        <input type="text"  v-model="nome" required><br>
 
-        <label for="">Email </label>
-        <input type="email" v-model="email" id=""><br>
+        <label>Email </label>
+        <input type="email" v-model="email" required><br>
 
-        <label for="">Qtd Funcionarios </label>
-        <input type="email" v-model="qtdfunc" id=""><br>
+        <label>Qtd Funcionarios </label>
+        <input type="number" v-model="qtdfunc"><br>
 
-        <label for="">Opinião </label>
-        <textarea v-model="opiniao"></textarea><br>
+        <label>Opinião </label>
+        <textarea v-model="opiniao" required></textarea><br>
 
-        <label for="">Tecnologias: </label><br>
+        <label>Tecnologias: </label><br>
         <label for="js">
           <input type="checkbox" v-model="tecnologias" value="javascript" id="js">
           JavaScript
@@ -44,22 +44,21 @@
           Intermediário
         </label>
         <label for="avancado">
-          <input type="radio" value="avançado" id="avancado" v-model="nivel">
+          <input type="radio" value="avançado" id="avancado" v-model="nivel" required>
           Avançado
         </label>
+        <button type="submit">Enviar</button>
       </form>
     </div>
     <hr>
-    <div class="item">
+    <div class="item" v-show="resultado">
       <h2>Usuario Cadastrado</h2>
       <label for="">Nome: {{ nome }}</label><br>
       <label for="">Email: {{ email }}</label><br>
       <label for="">Qtd Funcionarios: {{ qtdfunc }}</label><br>
       <label for="">Sua opinião: {{ opiniao }}</label><br>
       <label for="">Tecnologias: </label><br>
-      <ul>
-        <li v-for="tech in tecnologias" :key="tech"> {{ tech }}</li>
-      </ul>
+      <ul><li v-for="tech in tecnologias" :key="tech"> {{ tech }}</li></ul>
       <label for="">Tipo de contratação: {{ contratoSelected }}</label><br>
       <label for="">O seu nível é: {{ nivel }}</label>
     </div>
@@ -86,6 +85,14 @@ export default {
       ],
       contratoSelected: 'Freelancer',
       nivel: 'basico',
+      resultado: false,
+    }
+  },
+  methods:{
+    enviar(e){
+      e.preventDefault();
+      alert("Cadastrado com sucesso!")
+      this.resultado = true;
     }
   }
 }
