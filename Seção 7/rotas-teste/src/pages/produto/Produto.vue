0209befd411022ead6859ac09c2fe390e42a4c25
Leonardo Selegar <leonardo11132@gmail.com>
2023-02-27 23:18:02 -0300

    Navegação com metodos

--- Seção 7/rotas-teste/src/pages/produto/Produto.vue
+++ Seção 7/rotas-teste/src/pages/produto/Produto.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="produto">
     <h1>Tela Produto</h1>
+    <button @click="irHome">Ir para a home</button>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
-  name: 'Produto'
+  name: 'Produto',
+  methods:{
+    irHome(){
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
